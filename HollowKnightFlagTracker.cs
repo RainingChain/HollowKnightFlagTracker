f1c6fbba12776e4410826169333f3e7a565294e7
RainingChain <rainingchain@gmail.com>
2026-02-02 18:43:11 -0500

    Updated to use Lumafly

--- HollowKnightFlagTracker.cs
+++ HollowKnightFlagTracker.cs
@@ -1,91 +1,78 @@
-﻿using BepInEx;
-using BepInEx.Logging;
-using HarmonyLib;
+using Modding;
+using Modding.Converters;
+using System.Reflection;
 using System.Collections.Generic;
+using InControl;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using USceneManager = UnityEngine.SceneManagement.SceneManager;
 using System;
 using System.IO;
-using UnityEngine;
-using System.Linq;
-using Newtonsoft.Json.Linq;
 using GlobalEnums;
 
-// "D:\SteamLibrary\steamapps\common\Hollow Knight\BepInEx\LogOutput.log"
-
-/*
- * How to use:
- *  - Run node C:\Users\Samuel\source\repos\HollowKnightFlagTracker\watcher.js
- *  - Start HK game. 
- *  - Pause the game to print $REPO/saveData.json
- *  - Save file differences are printed in $REPO/flag_output.json
- * */
-
-[BepInPlugin("com.rainingchain.hollowknightflagtracker", "Hollow Knight Flag Tracker Mod", "1.0.0")]
-public class HollowKnightFlagTracker : BaseUnityPlugin
-{
-    public static string ROOT = "C:\\Users\\Samuel\\source\\repos\\HollowKnightFlagTracker";
-    public static string SAVE_DATA_OUTPUT = ROOT + "\\saveData.json";
-    public static bool WAS_PAUSED = false;
-    public static int COUNT = 0;
-
-    public static ManualLogSource Log;
-    public static GameManager gameManager;
-    public static System.Timers.Timer timer;
-    public void Awake()
+namespace HollowKnightFlagTracker {
+    public class HollowKnightFlagTracker : Mod
     {
-        Log = base.Logger;
+        public static string ROOT = "C:\\Users\\Samuel\\source\\repos\\HollowKnightFlagTracker";
+        public static string SAVE_DATA_OUTPUT = ROOT + "\\saveData.json";
+        public static bool WAS_PAUSED = false;
+        public static int COUNT = 0;
 
-        Logger.LogInfo("hollowknightmapper Plugin loaded and initialized.");
-        Harmony.CreateAndPatchAll(typeof(HollowKnightFlagTracker), null);
-    }
+        public static System.Timers.Timer timer;
 
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(GameManager), "Awake")]
-    public static void AwakePostfix(GameManager __instance)
-    {
-        HollowKnightFlagTracker.Log.LogInfo("init start");
+        public static string CreateSaveGameData()
+        {
+            GameManager.instance.SaveLevelState();
+            //PreparePlayerDataForSave.Invoke(HollowKnightFlagTracker.gameManager, new object[1] { HollowKnightFlagTracker.gameManager.profileID });
+            //GameManager.SaveGame()
+            string data = JsonUtility.ToJson((object)new SaveGameData(GameManager.instance.playerData, GameManager.instance.sceneData));
+            return data;
+        }
 
-        gameManager = __instance;
 
+        public HollowKnightFlagTracker(): base ("HollowKnightFlagTracker") { 
 
-        timer = new System.Timers.Timer();
-        timer.Interval = 40;
-        timer.AutoReset = true;
-        timer.Elapsed += (a, b) => {
-            if (COUNT < 1) {
-                HollowKnightFlagTracker.Log.LogInfo("Update");
-                COUNT = 1;
-            }
+        }
 
-            if (HollowKnightFlagTracker.gameManager.gameState == GameState.PAUSED)
-            {
-                if (WAS_PAUSED)
-                    return;
-                WAS_PAUSED = true;
-                HollowKnightFlagTracker.Log.LogInfo("Saved");
+        public override void Initialize() {
+            Log("Initializing");
 
-                var saveDataStr = CreateSaveGameData();
-                if (saveDataStr == null)
+            timer = new System.Timers.Timer();
+            timer.Interval = 40;
+            timer.AutoReset = true;
+            timer.Elapsed += (a, b) => {
+                if (COUNT < 1)
                 {
-                    HollowKnightFlagTracker.Log.LogInfo("saveData == null");
-                    return;
+                    //HollowKnightFlagTracker.Log.LogInfo("Update");
+                    COUNT = 1;
                 }
-                File.WriteAllText(SAVE_DATA_OUTPUT, saveDataStr);
 
-            } else
-            {
-                WAS_PAUSED = false;
-            }
-        };
-        timer.Start();
+                if (GameManager.instance.gameState == GameState.PAUSED)
+                {
+                    if (WAS_PAUSED)
+                        return;
+                    WAS_PAUSED = true;
+                    //HollowKnightFlagTracker.Log.LogInfo("Saved");
 
-        HollowKnightFlagTracker.Log.LogInfo("init end");
-    }
-    public static string CreateSaveGameData()
-    {
-        HollowKnightFlagTracker.gameManager.SaveLevelState();
-        //PreparePlayerDataForSave.Invoke(HollowKnightFlagTracker.gameManager, new object[1] { HollowKnightFlagTracker.gameManager.profileID });
-        //GameManager.SaveGame()
-        string data = JsonUtility.ToJson((object)new SaveGameData(HollowKnightFlagTracker.gameManager.playerData, HollowKnightFlagTracker.gameManager.sceneData));
-        return data;
+                    var saveDataStr = CreateSaveGameData();
+                    if (saveDataStr == null)
+                    {
+                        //HollowKnightFlagTracker.Log.LogInfo("saveData == null");
+                        return;
+                    }
+                    File.WriteAllText(SAVE_DATA_OUTPUT, saveDataStr);
+
+                }
+                else
+                {
+                    WAS_PAUSED = false;
+                }
+            };
+            timer.Start();
+
+            Log("Initialized");
+        }
+
+        public override string GetVersion() => GetType().Assembly.GetName().Version.ToString();
     }
 }
---
+++ README.md
@@ -0,0 +1,8 @@
+
+Mod for Hollow Knight that prints savefile modifications in a file.
+
+Modifications are printed upon pauding the game.
+
+Mod must be used with Lumafly.
+
+You must also install Node.js and run `node watcher.js`.
